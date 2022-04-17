92b11f517830b20f43f373e2f6ee8eec096633db
Franziska <franz@MacBook-Pro-von-Franziska.local>
2022-04-17 21:45:03 +0200

    Sets up empty project

---
+++ muesli/target/debug/deps/muesli-8626d7dfa7f46289.d
@@ -0,0 +1,5 @@
+/Users/franz/dev/muesli/muesli/target/debug/deps/muesli-8626d7dfa7f46289.rmeta: src/main.rs
+
+/Users/franz/dev/muesli/muesli/target/debug/deps/muesli-8626d7dfa7f46289.d: src/main.rs
+
+src/main.rs:
---
+++ muesli/target/debug/deps/muesli-ef1d22ef919ee9a2.d
@@ -0,0 +1,5 @@
+/Users/franz/dev/muesli/muesli/target/debug/deps/muesli-ef1d22ef919ee9a2.rmeta: src/main.rs
+
+/Users/franz/dev/muesli/muesli/target/debug/deps/muesli-ef1d22ef919ee9a2.d: src/main.rs
+
+src/main.rs:
